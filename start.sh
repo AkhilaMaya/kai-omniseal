417a8b0ce6cab0884fbbf68d52e1101704d70c72
AkhilaMaya <akhilamadireddy1987@gmail.com>
2025-05-17 05:22:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 kai_telegram.py &   # Launch Telegram bot in background
-python3 kai_omniseal.py     # Run main validation app in foreground
+python3 kai_telegram.py &   # background
+python3 kai_omniseal.py     # foreground
