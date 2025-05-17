3f5720b86078193246ae4ca595017ac90740c4ca
AkhilaMaya <akhilamadireddy1987@gmail.com>
2025-05-17 05:32:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Run both scripts in background
-python3 kai_telegram.py &
-python3 kai_omniseal.py &
-
-# Keep the container alive
-tail -f /dev/null
+# Run both scripts and log their outputs
+python3 kai_telegram.py > telegram.log 2>&1 &
+python3 kai_omniseal.py > flask.log 2>&1
