1093b9bbb2676ccf4611dee1a2b999da57054883
AkhilaMaya <akhilamadireddy1987@gmail.com>
2025-05-17 05:35:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# Run both scripts and log their outputs
+
+# Run both scripts and show logs
 python3 kai_telegram.py > telegram.log 2>&1 &
-python3 kai_omniseal.py > flask.log 2>&1
+python3 kai_omniseal.py > flask.log 2>&1 &
+
+# Print both logs continuously to console
+tail -f telegram.log flask.log
