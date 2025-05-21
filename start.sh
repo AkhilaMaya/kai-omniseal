c6430ecfbab30f8acfc9579ab259e384f89950cd
AkhilaMaya <akhilamadireddy1987@gmail.com>
2025-05-21 07:10:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 kai_telegram.py
+python3 kai_omniseal.py &   # Start backend Flask server in the background
+python3 kai_telegram.py     # Start Telegram bot (runs in foreground)
